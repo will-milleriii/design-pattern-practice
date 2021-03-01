03aadabefe8c608760c600a964e92bc0309170d3
Will Miller <willmiller@Wills-Air.fios-router.home>
2021-03-01 13:38:22 -0500

    fly behaviors added

---
+++ src/main/java/FlyNoWay.java
@@ -0,0 +1,6 @@
+public class FlyNoWay implements FlyBehavior{
+    // implement fly behavior for ducks that do not fly
+    public void fly() {
+        System.out.println("I can't fly!");
+    }
+}
---
+++ src/main/java/FlyWithWings.java
@@ -0,0 +1,6 @@
+public class FlyWithWings implements FlyBehavior{
+    //implement fly behavior for ducks that do fly
+    public void fly() {
+        System.out.println("I'm Flying!");
+    }
+}
