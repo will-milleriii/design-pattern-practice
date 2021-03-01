4d8d3484f6d7f3f866447adf1f637b78151a2251
Will Miller <willmiller@Wills-Air.fios-router.home>
2021-03-01 13:34:12 -0500

    abstract duck class

--- src/main/java/Duck.java
+++ src/main/java/Duck.java
@@ -1,3 +1,22 @@
 public abstract class Duck {
 
+    // declare two reference variables for the behavior interface types that all Duck subclasses inherit
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
+
+    public Duck(){}
+
+    public abstract void Display();
+
+    public void performFly(){
+        flyBehavior.fly();
+    }
+
+    public void performQuack(){
+        quackBehavior.quack();
+    }
+
+    public void swim(){
+        System.out.println("All ducks float, even decoys");
+    }
 }
