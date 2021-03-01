78af3a6ab5550c028bf9fd660ac8a3b3154dc956
Will Miller <willmiller@Wills-Air.fios-router.home>
2021-03-01 13:52:03 -0500

    added setters

--- src/main/java/Duck.java
+++ src/main/java/Duck.java
@@ -19,4 +19,14 @@ public abstract class Duck {
     public void swim(){
         System.out.println("All ducks float, even decoys");
     }
+
+    // create two setters, one for fly behavior and one for quack behavior
+
+    public void setFlyBehavior(FlyBehavior fb){
+        flyBehavior = fb;
+    }
+
+    public void setQuackBehavior(QuackBehavior qb){
+        quackBehavior = qb;
+    }
 }
